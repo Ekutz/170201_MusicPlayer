9ebd9ed94dd3bd3a7ed5051cb14066a8fbfbb180
Jinsoo Park <wistnsos@gmail.com>
2017-02-03 12:22:38 +0900

    seekbar changedListener 추가

--- app/src/main/java/com/jspark/android/musicplayer/PlayActivity.java
+++ app/src/main/java/com/jspark/android/musicplayer/PlayActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,12 +24,42 @@ public class PlayActivity extends AppCompatActivity {
     int position = 0;
     MediaPlayer audio;
     SeekBar sbar;
+    TextView duration, time;
 
     private static final int PLAY = 0;
     private static final int PAUSE = 1;
     private static final int STOP = 2;
 
     private static int playStatus = STOP;
+
+    private class OneSec extends Thread {
+
+        @Override
+        public void run() {
+            while (playStatus < STOP) {
+                if(audio!=null) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                sbar.setProgress(audio.getCurrentPosition());
+                                time.setText(String.format("%02d", audio.getCurrentPosition() / 1000 / 60) + ":" + String.format("%02d", (audio.getCurrentPosition() / 1000 - ((int) audio.getCurrentPosition() / 1000 / 60) * 60)));
+                            } catch(Exception e) {
+
+                            }
+                        }
+                    });
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch(InterruptedException e) {
+
+                }
+            }
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +73,8 @@ public class PlayActivity extends AppCompatActivity {
         btnPlay = (ImageButton)findViewById(R.id.btnPlay);
         btnFF = (ImageButton)findViewById(R.id.btnForwardF);
         sbar = (SeekBar)findViewById(R.id.seekBar);
+        duration = (TextView)findViewById(R.id.txtDu);
+        time = (TextView)findViewById(R.id.spendTime);
 
         btnRew.setOnClickListener(clickListener);
         btnPlay.setOnClickListener(clickListener);
@@ -53,14 +86,70 @@ public class PlayActivity extends AppCompatActivity {
 
         vp.setAdapter(adapter);
 
+        vp.addOnPageChangeListener(pageListener);
+
+        sbar.setOnSeekBarChangeListener(sbarListener);
+
         // 특정 페이지 호출
         Intent i = getIntent();
         if(i!=null) {
             Bundle bundle = i.getExtras();
             position = bundle.getInt("position");
             vp.setCurrentItem(position);
+
+            if(position==0) {
+                init();
+                duration.setText(String.format("%02d", music.get(position).length / 1000 / 60) + ":" + String.format("%02d", (music.get(position).length / 1000 - ((int) music.get(position).length / 1000 / 60) * 60)));
+                //time.setText(String.format("%02d",audio.getCurrentPosition()/1000/60)+":"+String.format("%02d",(audio.getCurrentPosition()/1000-((int)audio.getCurrentPosition()/1000/60)*60)));
+                time.setText("00:00");
+                play();
+            }
         }
     }
+
+    SeekBar.OnSeekBarChangeListener sbarListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            if(audio!=null&&b==true)
+            audio.seekTo(i);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    ViewPager.OnPageChangeListener pageListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            PlayActivity.this.position = position;
+            if(audio!=null) {
+                audio.release();
+            }
+
+            playStatus=STOP;
+            init();
+            play();
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+
 
     View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
@@ -80,15 +169,15 @@ public class PlayActivity extends AppCompatActivity {
     };
 
     private void play() {
-        Log.w("position", String.valueOf(position));
         switch(playStatus) {
             case STOP :
-                Uri uri = music.get(position).uri;
-                audio = MediaPlayer.create(this, uri);
-                audio.setLooping(false);
                 audio.start();
                 btnPlay.setImageResource(android.R.drawable.ic_media_pause);
                 playStatus=PLAY;
+
+                OneSec one = new OneSec();
+                one.start();
+
                 break;
             case PLAY :
                 audio.pause();
@@ -104,29 +193,13 @@ public class PlayActivity extends AppCompatActivity {
     }
 
     private void preview() {
-        audio.stop();
-        position -= 1;
+        if(position>0)position -= 1;
         vp.setCurrentItem(position);
-        Uri uri = music.get(position).uri;
-        audio = MediaPlayer.create(this, uri);
-        audio.setLooping(false);
-        audio.start();
-        btnPlay.setImageResource(android.R.drawable.ic_media_pause);
-        playStatus=PLAY;
-        Log.w("position", String.valueOf(position));
     }
 
     private void next() {
-        audio.stop();
-        position += 1;
+        if(position<music.size())position += 1;
         vp.setCurrentItem(position);
-        Uri uri = music.get(position).uri;
-        audio = MediaPlayer.create(this, uri);
-        audio.setLooping(false);
-        audio.start();
-        btnPlay.setImageResource(android.R.drawable.ic_media_pause);
-        playStatus=PLAY;
-        Log.w("position", String.valueOf(position));
     }
 
     @Override
@@ -137,4 +210,19 @@ public class PlayActivity extends AppCompatActivity {
         }
         playStatus = STOP;
     }
+
+    private void init() {
+        if(audio!=null) {
+            audio.release(); // 뷰페이저로 이동할 경우 미디어 해제하고 실행한다
+        }
+        Uri uri = music.get(position).uri;
+        audio = MediaPlayer.create(this, uri);
+        sbar.setMax(audio.getDuration());
+        duration.setText(String.format("%02d", audio.getDuration()/1000/60)+":"+String.format("%02d", (audio.getDuration()/1000-((int)audio.getDuration()/1000/60)*60)));
+        audio.setLooping(false);
+
+        Log.w("status", String.valueOf(playStatus));
+    }
+
+
 }
